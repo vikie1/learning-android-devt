ba8cfef1941223d46d08cec1f3a42526b56a08ac
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-08-11 19:25:52 +0300

    learning android development

--- learnIntents/app/src/main/java/com/example/learnintents/MainActivity.java
+++ learnIntents/app/src/main/java/com/example/learnintents/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.learnintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
         Intent msg = new Intent(Intent.ACTION_SEND);
         msg.setType("text/plain");
         msg.putExtra(Intent.EXTRA_TEXT,messageText);
-        startActivity(msg);
+        try {
+            startActivity(msg);
+        }catch (ActivityNotFoundException e){
+            Toast toast = Toast.makeText(this, "No activity can do that in your system", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
--- learnIntents/app/src/main/java/com/example/learnintents/ReceiveMessageActivity.java
+++ learnIntents/app/src/main/java/com/example/learnintents/ReceiveMessageActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class ReceiveMessageActivity extends AppCompatActivity {A
+public class ReceiveMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
