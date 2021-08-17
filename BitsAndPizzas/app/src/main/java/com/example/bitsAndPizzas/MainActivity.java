e88468e119153686cf5e580788d057f85ed57e42
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-08-17 10:23:24 +0300

    learning android davelopment

--- BitsAndPizzas/app/src/main/java/com/example/bitsAndPizzas/MainActivity.java
+++ BitsAndPizzas/app/src/main/java/com/example/bitsAndPizzas/MainActivity.java
@@ -5,6 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,15 +20,45 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ShareActionProvider shareActionProvider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        SectionPagerAdapter sectionPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        ViewPager2 pager = (ViewPager2) findViewById(R.id.pager);
+        //pager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        pager.setAdapter(sectionPagerAdapter);
+        //pager.setPageTransformer(new MarginPageTransformer(1500));
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        new TabLayoutMediator(tabLayout, pager,
+                ((tab, position) -> {
+                    switch (position){
+                        case 0:
+                            tab.setText(getResources().getText(R.string.home_tab));
+                            break;
+                        case 1:
+                            tab.setText(getResources().getText(R.string.pizza_tab));
+                            break;
+                        case 2:
+                            tab.setText(getResources().getText(R.string.pasta_tab));
+                            break;
+                        case 3:
+                            tab.setText(getResources().getText(R.string.store_tab));
+                            break;
+                    }
+                })
+        ).attach();
     }
 
     @Override
@@ -50,6 +88,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 return true;
             default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class SectionPagerAdapter extends FragmentStateAdapter{
+
+        public SectionPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 0:
+                    return new TopFragment();
+                case 1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return new StoresFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 4;
         }
     }
 }
